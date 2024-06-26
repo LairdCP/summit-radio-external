e1ee1f255921430cd509defb35e770ab45f2c593
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2024-06-26 12:18:57 -0400

    brcm_patchram_plus: Use https to for external download
    PROD-12230

--- package/summit-brcm-patchram-plus/summit-brcm-patchram-plus.mk
+++ package/summit-brcm-patchram-plus/summit-brcm-patchram-plus.mk
@@ -7,7 +7,6 @@
 SUMMIT_BRCM_PATCHRAM_PLUS_VERSION = $(SUMMIT_LWB_RADIO_STACK_VERSION_VALUE)
 SUMMIT_BRCM_PATCHRAM_PLUS_LICENSE = Apache-2.0
 SUMMIT_BRCM_PATCHRAM_PLUS_LICENSE_FILES = LICENSE
-SUMMIT_BRCM_PATCHRAM_PLUS_SITE_METHOD = git
 
 ifeq ($(SUMMIT_BRCM_PATCHRAM_PLUS_VERSION),0.0.0.0)
   SUMMIT_BRCM_PATCHRAM_PLUS_VERSION = origin/master
@@ -15,8 +14,9 @@ endif
 
 ifeq ($(MSD_BINARIES_SOURCE_LOCATION),laird_internal)
   SUMMIT_BRCM_PATCHRAM_PLUS_SITE = git@github.com:rfpros/cp_linux-brcm_patchram.git
+  SUMMIT_BRCM_PATCHRAM_PLUS_SITE_METHOD = git
 else
-  SUMMIT_BRCM_PATCHRAM_PLUS_SITE = git@github.com:LairdCP/brcm_patchram.git
+  SUMMIT_BRCM_PATCHRAM_PLUS_SITE = $(call github,LairdCP,brcm_patchram,$(SUMMIT_BRCM_PATCHRAM_PLUS_VERSION))
 endif
 
 ifeq ($(BR2_PACKAGE_BLUEZ4_UTILS),y)
@@ -30,7 +30,7 @@ define SUMMIT_BRCM_PATCHRAM_PLUS_BUILD_CMDS
 endef
 
 define SUMMIT_BRCM_PATCHRAM_PLUS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 -t $(TARGET_DIR)/usr/bin/ $(@D)/brcm_patchram_plus
+	$(INSTALL) -D -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/brcm_patchram_plus
 endef
 
 $(eval $(generic-package))
