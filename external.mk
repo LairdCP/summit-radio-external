77acc105a590e6d57c5f6d88498e315fd7b5a437
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2025-02-10 08:26:10 -0500

    Updated to Ezurio organization

--- external.mk
+++ external.mk
@@ -4,15 +4,15 @@ SUMMIT_RADIO_STACK_ARCH = $(call qstrip,$(BR2_PACKAGE_SUMMIT_RADIO_STACK_ARCH))
 
 RFPROS_FILESHARE_AUTH ?= $(if $(RFPROS_FILESHARE_USER),$(RFPROS_FILESHARE_USER):$(RFPROS_FILESHARE_PASS)@,)
 
-SUMMIT_RADIO_URI_BASE_60       = https://github.com/LairdCP/Sterling-60-Release-Packages/releases/download/LRD-REL
-SUMMIT_RADIO_URI_BASE_BDSDMAC  = https://github.com/LairdCP/BDSDMAC-Release-Packages/releases/download/LRD-REL
-SUMMIT_RADIO_URI_BASE_LWB      = https://github.com/LairdCP/Sterling-LWB-and-LWB5-Release-Packages/releases/download/LRD-REL
-SUMMIT_RADIO_URI_BASE_NX       = https://github.com/LairdCP/SonaNX-Release-Packages/releases/download/LRD-REL
-SUMMIT_RADIO_URI_BASE_TI       = https://github.com/LairdCP/SonaTI-Release-Packages/releases/download/LRD-REL
+SUMMIT_RADIO_URI_BASE_60       = https://github.com/Ezurio/Sterling-60-Release-Packages/releases/download/LRD-REL
+SUMMIT_RADIO_URI_BASE_BDSDMAC  = https://github.com/Ezurio/BDSDMAC-Release-Packages/releases/download/LRD-REL
+SUMMIT_RADIO_URI_BASE_LWB      = https://github.com/Ezurio/Sterling-LWB-and-LWB5-Release-Packages/releases/download/LRD-REL
+SUMMIT_RADIO_URI_BASE_NX       = https://github.com/Ezurio/SonaNX-Release-Packages/releases/download/LRD-REL
+SUMMIT_RADIO_URI_BASE_TI       = https://github.com/Ezurio/SonaTI-Release-Packages/releases/download/LRD-REL
 SUMMIT_RADIO_URI_BASE_INTERNAL = https://$(RFPROS_FILESHARE_AUTH)files.devops.rfpros.com/builds/linux
 
 ifeq ($(SUMMIT_SOM_URI_BASE_ARCHIVE),)
-SUMMIT_RADIO_URI_BASE_MSD      = https://github.com/LairdCP/Legacy-Release-Packages/releases/download/LRD-REL
+SUMMIT_RADIO_URI_BASE_MSD      = https://github.com/Ezurio/Legacy-Release-Packages/releases/download/LRD-REL
 else
 SUMMIT_RADIO_URI_BASE_MSD      = $(SUMMIT_SOM_ARCHIVE_URI_BASE)
 endif
